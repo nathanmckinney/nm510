d89062dbb20494cb99da5fc6adc4750e2dbcf2c1
Nathan McKinney <mckinney@utk.edu>
2021-02-26 22:49:38 -0500

    init commit

---
+++ tests/test_nm510.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+
+"""Tests for `nm510` package."""
+
+
+import unittest
+
+from nm510 import nm510
+
+
+class TestNm510(unittest.TestCase):
+    """Tests for `nm510` package."""
+
+    def setUp(self):
+        """Set up test fixtures, if any."""
+
+    def tearDown(self):
+        """Tear down test fixtures, if any."""
+
+    def test_000_something(self):
+        """Test something."""
